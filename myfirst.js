5f1901db4a67fc0176d682fe78c724f640607e16
Kevin Meixner <kmeixner42@gmail.com>
2025-05-23 20:01:05 -0400

    Renamed module to something more appropriate
    renamed:    myfirstmodule.js -> mydatemodule.js
    modified:   myfirst.js

--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,6 @@
 var http = require('http');
-var dt = require('./myfirstmodule'); // date module I made
-var uc = require('upper-case'); // node module via npm install upper-case
+var dt = require('./mydatemodule'); // date module I made
+var uc = require('upper-case'); // node module via "npm install upper-case"
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
